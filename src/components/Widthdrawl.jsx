313733f533c817798fe0fc031d6d1e12e127b92b
Arnab Biswas <khanp4397@gmail.com>
2021-11-21 16:10:01 +0530

    Pushy

--- src/components/Widthdrawl.jsx
+++ src/components/Widthdrawl.jsx
@@ -1,51 +1,31 @@
 import axios from "axios";
 import { useState } from "react";
-import { Form } from "react-bootstrap";
 
 function Widthdrawl() {
     const user = JSON.parse(localStorage.getItem("user"));
-    const [widthdrawlAmount, setWidthdrawlAmount] = useState(0)
     const [amount, setAmount] = useState(0)
     const [balance, setBalance] = useState(user.wallet.main_wallet)
     const [success, setSuccess] = useState(false)
-    const [mainBalance, setMainBalance] = useState(user.wallet.main_wallet);
-    const [loading, setLoading] = useState(false);
 
     const withdrawlBalance = (e) => {
         e.preventDefault();
-        axios({
-            method: "POST",
-            data: {
-                amount: widthdrawlAmount,
-                token: JSON.parse(localStorage.getItem("token"))
-            },
-            withCredentials: true,
-            url: "https://stormy-ridge-27884.herokuapp.com/withdraw_from_main",
-        }).then((response) => {
+
+        axios.post("https://stormy-ridge-27884.herokuapp.com/withdraw_from_main", {
+            amount: amount,
+            token: localStorage.getItem("token")
+        }).then(response => {
             user.wallet = response.data.balance;
             localStorage.setItem("user", JSON.stringify(user));
             setBalance(response.data.balance.main_wallet)
             setSuccess(true)
-        });
-    };
+        }).catch(err => {
+            console.log(err)
+        })
+    }
 
 
         
 
-    const transferBalance = (e) => {
-        e.preventDefault();
-        axios.post("https://stormy-ridge-27884.herokuapp.com/transfer_to_activation", {
-            token: JSON.parse(localStorage.getItem("token")),
-            amount: parseInt(amount)
-        }).then(res => {
-            user.wallet = res.data.balance;
-            setMainBalance(res.data.balance.main_wallet);
-            localStorage.setItem("user", JSON.stringify(user));
-            setLoading(false);
-        }).catch(err => {
-                console.log(err);
-        })
-    }
 
     return (
         <div className="section">
@@ -71,7 +51,7 @@ function Widthdrawl() {
             {/* Widthdrawl Form */}
             <h1 className="withd-head">Withdrawal Balance</h1>
        
-            <form onSubmit={(e) => withdrawlBalance(e)}>
+            <form onSubmit={withdrawlBalance}>
                 <div className="form-group" key="formAvailableBalance">
                     <label>Available Balance</label>
                     <input
@@ -89,8 +69,8 @@ function Widthdrawl() {
                         type="number"
                         className="form-control text-left pl-2 w-100"
                         placeholder="Enter Amount"
-                        value={0 || widthdrawlAmount}
-                        onChange={(e) => setWidthdrawlAmount(e.target.value)}
+                        value={0 || amount}
+                        onChange={(e) => setAmount(e.target.value)}
                     />
                 </div>
 
@@ -115,46 +95,71 @@ function Widthdrawl() {
                 </button>
             </form>
 
-             <Form onSubmit={transferBalance} >
-                 <h1>Main Wallet to Activation Wallet</h1>
-                <div className="form-group" key="formMainBalance">
-                    <label>Main Wallet Balance:</label>
-                    <input
-                        type="text"
-                        className="form-control text-left"
-                        placeholder="Main Wallet Balance"
-                        value={mainBalance}
-                        disabled
-                    />
-                </div>
-
-                <div className="form-group" key="formAmount">
-                    <label>Amount:</label>
-                    <input
-                        type="text"
-                        className="form-control w-100 text-left"
-                        onChange={(e) => setAmount(e.target.value)}
-                        placeholder="Amount"
-                        value={amount}
-                    />
-                </div>
-
-
-                <button
-                    className={`btn btn-primary ml-3 ${loading ? "disabled" : ""}`}
-                    onClick={() => setLoading(true)}
-                >
-                    {loading ? "Loading..." : "Transfer"}
-                </button>
-
-            </Form>
-            <br/>
-            <br/>
-            <br />
-       
+                <WithdrawlMainToActivation/>
             
         </div>
     );
+}
+
+
+const WithdrawlMainToActivation = () => {
+     const user = JSON.parse(localStorage.getItem("user"));
+    const [amount, setAmount] = useState(0)
+    const [mainBalance, setMainBalance] = useState(user.wallet.main_wallet);
+    const [loading, setLoading] = useState(false);
+
+
+    const transferBalance = (e) => {
+        e.preventDefault();
+        axios.post("https://stormy-ridge-27884.herokuapp.com/transfer_to_activation", {
+            token: localStorage.getItem("token"),
+            amount: parseInt(amount)
+        }).then(res => {
+            user.wallet = res.data.balance;
+            setMainBalance(res.data.balance.main_wallet);
+            localStorage.setItem("user", JSON.stringify(user));
+            setLoading(false);
+            alert(res.data.message)
+        }).catch(err => {
+                console.log(err);
+        })
+    }
+
+    return (
+        <form onSubmit={transferBalance} >
+        <h1>Main Wallet to Activation Wallet</h1>
+        <div className="form-group" key="formMainBalance">
+            <label>Main Wallet Balance:</label>
+            <input
+                type="text"
+                className="form-control text-left"
+                placeholder="Main Wallet Balance"
+                value={mainBalance}
+                disabled
+            />
+        </div>
+
+        <div className="form-group" key="formAmount">
+            <label>Amount:</label>
+            <input
+                type="text"
+                className="form-control w-100 text-left"
+                onChange={(e) => setAmount(e.target.value)}
+                placeholder="Amount"
+                value={amount}
+            />
+        </div>
+
+
+        <button
+            className={`btn btn-primary ml-3 ${loading ? "disabled" : ""}`}
+            onClick={() => setLoading(true)}
+        >
+            {loading ? "Loading..." : "Transfer"}
+        </button>
+
+    </form>
+    )
 }
 
 export default Widthdrawl;
